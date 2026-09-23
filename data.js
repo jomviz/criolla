54c4bcb4cb7f2340a3d59f7e43be5f698f326172
JOMVIZ <mariaclaudiapetit@gmail.com>
2026-09-23 18:06:48 -0300

    Act20260923

---
+++ data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-vereda",
+      "name": "1 Vereda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5104838261685121,
+          "pitch": 0.0896603990608078,
+          "rotation": 0,
+          "target": "1-2-galeria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-galeria",
+      "name": "2 Galeria",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4713252041092062,
+          "pitch": 0.4053147582416763,
+          "rotation": 0,
+          "target": "2-3-cocina-comedor"
+        },
+        {
+          "yaw": 0.017103047200382804,
+          "pitch": 0.16362236421046816,
+          "rotation": 0,
+          "target": "3-4-bao-1"
+        },
+        {
+          "yaw": 0.426704990635951,
+          "pitch": 0.09119520082157351,
+          "rotation": 0,
+          "target": "6-7-dormitorio-principal"
+        },
+        {
+          "yaw": 0.32237599075851087,
+          "pitch": 0.11107073689604618,
+          "rotation": 0,
+          "target": "5-6-dormitorio-2"
+        },
+        {
+          "yaw": -2.4590437910908385,
+          "pitch": 0.24212273639973603,
+          "rotation": 0,
+          "target": "0-1-vereda"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-cocina-comedor",
+      "name": "3 Cocina Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08129565436889408,
+          "pitch": 0.20407558588478025,
+          "rotation": 0,
+          "target": "3-4-bao-1"
+        },
+        {
+          "yaw": 1.9382349449281557,
+          "pitch": 0.36032539238399863,
+          "rotation": 0,
+          "target": "1-2-galeria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4-bao-1",
+      "name": "4 Baño 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0071016374324566,
+          "pitch": 0.9877065813518229,
+          "rotation": 0,
+          "target": "4-5-dormitorio-1"
+        },
+        {
+          "yaw": 1.8601243731070456,
+          "pitch": 1.0959129143765498,
+          "rotation": 0,
+          "target": "2-3-cocina-comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5-dormitorio-1",
+      "name": "5 Dormitorio 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5031551796322358,
+          "pitch": 0.49533058562404797,
+          "rotation": 0,
+          "target": "5-6-dormitorio-2"
+        },
+        {
+          "yaw": 2.2662329727579795,
+          "pitch": 0.5169285111718835,
+          "rotation": 0,
+          "target": "3-4-bao-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6-dormitorio-2",
+      "name": "6 Dormitorio 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.813224743356983,
+          "pitch": 0.5197089414093661,
+          "rotation": 0,
+          "target": "4-5-dormitorio-1"
+        },
+        {
+          "yaw": -2.5767142552160465,
+          "pitch": 0.563038282144456,
+          "rotation": 0,
+          "target": "6-7-dormitorio-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7-dormitorio-principal",
+      "name": "7 Dormitorio principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.097626610592531,
+          "pitch": 0.8536991709831341,
+          "rotation": 0,
+          "target": "7-8-bao-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-8-bao-2",
+      "name": "8 Baño 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4522835420896847,
+          "pitch": 0.60799048364977,
+          "rotation": 0,
+          "target": "6-7-dormitorio-principal"
+        },
+        {
+          "yaw": 2.1849453408728294,
+          "pitch": 0.6640835647038958,
+          "rotation": 0,
+          "target": "5-6-dormitorio-2"
+        },
+        {
+          "yaw": 2.2526681375232602,
+          "pitch": 0.4575285146102921,
+          "rotation": 0,
+          "target": "4-5-dormitorio-1"
+        },
+        {
+          "yaw": 2.3219005013612612,
+          "pitch": 0.29332834430878485,
+          "rotation": 0,
+          "target": "3-4-bao-1"
+        },
+        {
+          "yaw": 2.384814528176163,
+          "pitch": 0.17094994925719043,
+          "rotation": 0,
+          "target": "2-3-cocina-comedor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
